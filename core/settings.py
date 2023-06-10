bb8428cc50706f47fbe042508e62068583f4ab5c
aglili <cecilselorm34@gmail.com>
2023-06-10 23:32:17 +0000

    deployment fix

--- core/settings.py
+++ core/settings.py
@@ -130,6 +130,9 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATIC_ROOT = 'blog\images'
+
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
