496c63ef4dac23499d449de124e13e6d2ce5c06c
aglili <cecilselorm34@gmail.com>
2023-06-10 22:34:59 +0000

    added swagger_docs

--- core/urls.py
+++ core/urls.py
@@ -9,11 +9,11 @@ from drf_yasg import openapi
 
 schema_view = get_schema_view(
     openapi.Info(
-        title="Your API",
+        title="BLOG API",
         default_version='v1',
-        description="API documentation for Your Project",
+        description="CRUD BLOG API ",
         terms_of_service="https://www.example.com/policies/terms/",
-        contact=openapi.Contact(email="contact@example.com"),
+        contact=openapi.Contact(email="cecilselorm34@gmail.com"),
         license=openapi.License(name="BSD License"),
     ),
     public=True,
@@ -22,7 +22,7 @@ schema_view = get_schema_view(
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("api/",include("accounts.urls")),
+    path("accounts/",include("accounts.urls")),
     path("",include("blog.urls")),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
